3540309863e9442f96c8ccdb06c7ce7b32bb389d
Ashutosh <lakharaashutosh5@gmail.com>
2026-01-18 20:37:13 +0530

    Fixed main JS

--- .github/actions/js-action/main.js
+++ .github/actions/js-action/main.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec')
-(function run() {
+function run() {
     core.notice("This is from my Custom JS Action main file");
-})();
+};
+
+run();
